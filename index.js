377b4556300ee46b3b9ee1317f9e695bcc6b677c
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-12 16:49:56 +0300

    submits array map labs

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorialTitle => {
+    return tutorialTitle.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() +  word.slice(1);
+    }).join(' ');
+  });
 }
